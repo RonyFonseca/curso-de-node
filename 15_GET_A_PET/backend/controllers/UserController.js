1bc23115581bb627ee93b7f45784a8d87045935f
Rony Dev <ronyfonsecadelima24@gmail.com>
2025-01-04 11:56:37 -0300

    Parei em registrando usuario em sistema

--- 15_GET_A_PET/backend/controllers/UserController.js
+++ 15_GET_A_PET/backend/controllers/UserController.js
@@ -1,136 +1,60 @@
 import User from "../models/User.js"
 import bcrypt from "bcrypt"
-import CreateUserToken from "../helpers/create-user-token.js"
-import getToken from "../helpers/get-token.js"
-import jwt from "jsonwebtoken"
 
 class UserController{
     static async register(req, res){
-        const {name, email, password, image, phone, confirmpassword} = req.body
+        const {name, phone, email, password, confirmPassword} = req.body 
 
-        // VALIDATIONS 
+        //VALIDATIONS
         if(!name){
-            res.status(422).json({message:"O nome é obrigatório"})
+            res.status(422).json({message:"The name field is empty!"})
             return 
         }
-
         if(!phone){
-            res.status(422).json({message:"O telefone é obrigatório"})
+            res.status(422).json({message:"The phone field is empty!"})
+            return 
+        }
+        if(!email){
+            res.status(422).json({message:"The email field is empty!"})
+            return 
+        }
+        if(!password){
+            res.status(422).json({message:"The password field is empty!"})
+            return 
+        }
+        if(!confirmPassword){
+            res.status(422).json({message:"The confirm password field is empty!"})
+            return 
+        }
+        if(password !== confirmPassword){
+            res.status(422).json({message:"The field password is different from the confirmation of password field!"})
             return 
         }
         
-        if(!email){
-            res.status(422).json({message:"O email é obrigatório"})
+        const UserExist = await User.findOne({email:email})
+        if(UserExist){
+            res.status(422).json({message:"This user already exist!"})
             return 
         }
 
-        if(!password){
-            res.status(422).json({message:"A senha é obrigatória"})
-            return 
-        }
-
-        if(!confirmpassword){
-            res.status(422).json({message:"A senha é obrigatória"})
-            return 
-        }
-
-        if(password != confirmpassword){
-            res.status(422).json({message: "A senha e a confirmação precisam ser iguais!"})
-            return
-        }
-
-        //check if user exist 
-        const userExist = await User.findOne({email:email})
-        if(userExist){
-            res.status(422).json({message: "Este usuário ja existe"})
-            return
-        }
-
-
-        // Criação do usuário 16/12/2024
-        // Creat a password
+        //Creating the password 
         const salt = await bcrypt.genSalt(12)
         const passwordHash = await bcrypt.hash(password, salt)
 
-        // Creat a user
+        //Creating the user
         const user = new User({
-            name, 
-            email,
-            phone, 
-            password: passwordHash
+            name: name, 
+            email: email, 
+            password: passwordHash,
+            phone: phone
         })
 
-
         try {
-
             const newUser = await user.save()
-            await CreateUserToken(newUser, req, res)
-        } catch(error){
+            res.status(201).json({message: "Creation was a success !", newUser})
+        }catch(error){
             res.status(500).json({message: error})
         }
-        
-    }
-
-    static async login(req, res){
-        const {email, password} = req.body 
-
-        if(!email){
-            res.status(422).json({message:"O email é obrigatório"})
-            return 
-        }
-
-        if(!password){
-            res.status(422).json({message:"A senha é obrigatória"})
-            return 
-        }
-
-        const user = await User.findOne({email:email})
-        if(!user){
-            res.status(422).json({message:"Esse usuário não existe !"})
-            return
-        }
-
-        // confirm password
-        const passwordExist = await bcrypt.compare(password, user.password)
-        if(!passwordExist){
-            res.status(422).json({message:"Senha errada !"})
-            return
-        }
-
-        // login with token
-        await CreateUserToken(user, req, res)
-    }
-
-    static async checkUser(req, res){
-        let currentUser 
-
-        if(req.headers.authorization){
-            const token = getToken(req) // VAI PEGAR O TOKEN VINDO DE GETTOKEN 
-            const decoded = jwt.verify(token, "nossosecret") // VAI DECODIFICAR O TOKEN COM O SECRET
-            currentUser = await User.findById(decoded.id) // VAI PROCURAR O USUARIO PELO ID
-            currentUser.password = undefined // E ZERAR A SENHA POR MOTIVO DE SEGURANÇA
-        } else {
-            currentUser = null
-        }
-
-        res.status(200).send(currentUser)
-    }
-
-    static async getUserById(req, res){
-        const id = req.params.id
-
-        const user = await User.findById(id).select("-password")
-
-        if(!user){
-            res.status(422).json({message:"Usuário não encontrado!"})
-            return
-        }
-
-        res.status(200).json({user})
-    }
-
-    static async editUser(req, res){
-        res.status(200).json({message: "Update deu certo !"})
     }
 }
 
--- 15_GET_A_PET/backend/routes/UserRoutes.js
+++ 15_GET_A_PET/backend/routes/UserRoutes.js
@@ -1,15 +1,9 @@
 import express from "express"
 
 import UserController from "../controllers/UserController.js"
-import User from "../models/User.js"
-import verifyToken from "../helpers/verify-token.js"
 
 const router = express.Router()
 
 router.post("/register", UserController.register)
-router.post("/login", UserController.login)
-router.get("/checkuser", UserController.checkUser)
-router.get("/:id", UserController.getUserById)
-router.patch("/edit/:id", verifyToken, UserController.editUser)
 
 export default router
